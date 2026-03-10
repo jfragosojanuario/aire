7f667e827b13b27d41d7980bcc807dfc144c2132
Joao Januario <joao.fragoso.januario@gmail.com>
2026-03-10 17:37:44 +0000

    database amended

--- app/main.py
+++ app/main.py
@@ -86,7 +86,7 @@ def load_model() -> SentenceTransformer:
 
 @st.cache_data(show_spinner=False)
 def load_df_and_embeddings() -> Tuple[pd.DataFrame, np.ndarray]:
-    df = pd.read_excel(REPO_ROOT / "database" / "Data AIRE pROJECT.xlsx")
+    df = pd.read_excel(REPO_ROOT / "database" / "Data AIRE project.xlsx")
     df["combined_text_df"] = (
         df["Domain"].fillna("")
         + " "
